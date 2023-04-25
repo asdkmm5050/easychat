175ca3f2047b8a2daaf1d9e3e1de29dea0bf7f94
asdkmm5050 <asdkmm505@gmail.com>
2023-04-26 02:45:37 +0800

    Create ChatRoom.java

---
+++ src/main/java/com/example/easychat/entity/ChatRoom.java
@@ -0,0 +1,28 @@
+package com.example.easychat.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "chat_room")
+@Data
+public class ChatRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "creater_id")
+    private int createrId;
+
+    @Column(name = "create_time")
+    private String createTime = LocalDateTime.now().toString();
+
+
+
+}
