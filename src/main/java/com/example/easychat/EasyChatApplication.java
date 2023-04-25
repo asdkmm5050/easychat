a2c8c7353350535e531bc9086480550aa8527fb5
asdkmm5050 <asdkmm505@gmail.com>
2023-04-25 22:44:22 +0800

    Create EasyChatApplication.java

---
+++ src/main/java/com/example/easychat/EasyChatApplication.java
@@ -0,0 +1,12 @@
+package com.example.easychat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyChatApplication.class, args);
+    }
+}
