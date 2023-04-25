f68e5f8f8a55262845d73987e115f5c4612df442
asdkmm5050 <asdkmm505@gmail.com>
2023-04-25 22:47:04 +0800

    Create SecurityConfiguration.java

---
+++ src/main/java/com/example/easychat/utils/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.example.easychat.utils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(new AuthorizationCheckFilter(), BasicAuthenticationFilter.class);
+        return http.build();
+    }
+
+
+}
