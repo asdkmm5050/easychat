3504299f2a2618ef2a5b2da885cbe6b44d333b01
asdkmm5050 <asdkmm505@gmail.com>
2023-04-25 22:47:20 +0800

    Create FriendService.java

---
+++ src/main/java/com/example/easychat/service/FriendService.java
@@ -0,0 +1,47 @@
+package com.example.easychat.service;
+
+import com.example.easychat.entity.Friend;
+import com.example.easychat.repository.FriendRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class FriendService {
+
+    private final FriendRepository friendRepository;
+
+
+    public Optional<Friend> getFriend(int userId, int friendId) {
+        return friendRepository.findByUserIdAndFriendId(userId, friendId);
+    }
+
+    public List<Friend> getFriendList(int userId) {
+
+        return friendRepository.findAllByUserId(userId);
+
+    }
+
+    public boolean addFriend(int userId, int friendId) {
+        Optional<Friend> friend = friendRepository.findByUserIdAndFriendId(userId, friendId);
+        if (friend.isPresent()) {
+            return false;
+        } else {
+            Friend newFriend = new Friend(userId, friendId);
+            friendRepository.save(newFriend);
+            return true;
+        }
+    }
+
+
+    public void deleteFriend(int id) {
+
+        friendRepository.deleteById(id);
+
+    }
+
+
+}
