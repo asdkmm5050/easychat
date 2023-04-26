1534dec6c5f32bd3747334d733c3a6fa5ca855ab
asdkmm5050 <asdkmm505@gmail.com>
2023-04-26 21:29:13 +0800

    Change folder

--- src/main/java/com/example/easychat/config/SecurityConfiguration.java
+++ src/main/java/com/example/easychat/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
-package com.example.easychat.utils;
+package com.example.easychat.config;
 
+import com.example.easychat.utils.AuthorizationCheckFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,8 +14,8 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.addFilterBefore(new AuthorizationCheckFilter(), BasicAuthenticationFilter.class);
+        //http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        //http.addFilterBefore(new AuthorizationCheckFilter(), BasicAuthenticationFilter.class);
         return http.build();
     }
 
